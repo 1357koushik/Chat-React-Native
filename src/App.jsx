beb0e9a214b6f57376dbd509e38966a2f6389bdb
Moram Koushik <1357koushik@gmail.com>
2025-10-19 13:02:59 +0530

    Base app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function DMs(props) {
+  const pmail=props.mail;
+  const 
+  return()
+}
+
+function App() {
+
+  return ()
+}
+
+export default App
